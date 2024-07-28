4507d5227a81a82ec819d445c458a42250a746a7
Vivetha Baskaran <vivethaitech@gmail.com>
2024-07-28 18:04:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const button = document.getElementById('btn')
 
-button.addEventListener('click',
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+     valid();
+})
+
     function valid(){
-        //let number = parseInt(document.getElementById('num').value)
-        //let numberString = number.toString();
-       // let numberArray = numberString.split('');
-       // a = numberArray.map(number);
-       // let result = document.getElementById('result')
        let number = document.getElementById('num').value;
+
+       if (number === "") {
+        alert('Number is required.');
+        return;
+    }
        let numberArray = number.split('').map(Number);
        let result = document.getElementById('result');  
-       if (numberArray.some(isNaN)) {
-           result.innerHTML = "Please enter a valid number";
-           return;
-       }
+ 
        let sum = 0;
        for (let i = 0; i < numberArray.length; i++) {
            let num = numberArray[numberArray.length - 1 - i];
@@ -31,4 +32,4 @@ button.addEventListener('click',
             result.innerHTML="This is not a valid credit card number";
         }
     }
-)
+  
